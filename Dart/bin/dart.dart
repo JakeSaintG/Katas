12ea08ba100b66acd1937fcbed77976dc42ec4f8
Jake Saint Germain <jakesaint27@gmail.com>
2023-01-06 10:52:29 -0500

    dart kata project set up

---
+++ Dart/bin/dart.dart
@@ -0,0 +1,7 @@
+import 'package:dart/katas/clock.dart' as clock;
+
+void main(List<String> arguments) {
+  
+  print(clock.past(0, 1, 1)); // More in tests
+
+}
---
+++ Dart/lib/katas/clock.dart
@@ -0,0 +1,6 @@
+int past(int h, int m, int s) {
+
+  // WORK IN PROGRESS! Getting up project.
+
+  return 61000; // Final answer for main
+}
