d79c8910daae04f4a2ddc4fb1049555c8256c382
Jake Saint Germain <jakesaint27@gmail.com>
2023-01-07 23:47:14 -0500

    Clock kata in dart

--- Dart/lib/katas/clock.dart
+++ Dart/lib/katas/clock.dart
@@ -1,6 +1,26 @@
+// Codewars
+
+/*=========================================================================================
+    Clock shows h hours, m minutes and s seconds after midnight
+    Your task is to write a function which returns the time since midnight in milliseconds.
+*/
+
+//Not pleased with this solution...
 int past(int h, int m, int s) {
-
-  // WORK IN PROGRESS! Getting up project.
-
-  return 61000; // Final answer for main
+  return DateTime.parse('1993-01-27 00:00:00Z')
+          .add(Duration(hours: h))
+          .add(Duration(minutes: m))
+          .add(Duration(seconds: s))
+          .millisecondsSinceEpoch -
+      DateTime.parse('1993-01-27 00:00:00Z').millisecondsSinceEpoch;
 }
+
+/*
+  Notes:
+
+  I should have used Duration:
+
+  int past(int h, int m, int s) {
+    return new Duration(hours: h, minutes: m, seconds: s).inMilliseconds;
+  }
+*/
