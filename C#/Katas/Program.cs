0f8633a826d816f3ae0e5d542d7c82f79ed49acb
JakeSaintG <jakestgermain@outlook.com>
2023-01-15 18:55:35 -0500

    square sum kata in C#

--- C#/Katas/Program.cs
+++ C#/Katas/Program.cs
@@ -6,8 +6,10 @@
 
 //Console.WriteLine(DuplicateEncoder.DuplicateEncode("Success"));
 
-// Kata didn't want it to be static... Who am I to say no?
+/*====Kata didn't want it to be static... Who am I to say no?====*/
 //DigitalRoot dr = new DigitalRoot();
 //Console.WriteLine(dr.ReturnDigitalRoot(493193));
 
-Console.WriteLine(Smash.SmashSentance(new string[] { "hello", "world" }));
+//Console.WriteLine(Smash.SmashSentance(new string[] { "hello", "world" }));
+
+Console.WriteLine(Square.SquareSum(new int[] { 1, 2, 3, 4, 5 }));
