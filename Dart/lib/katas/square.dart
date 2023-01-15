3b36f72ceea0676d0d818996fed3a9a8fd9ca519
JakeSaintG <jakestgermain@outlook.com>
2023-01-15 18:57:13 -0500

    square kata in dart set up

---
+++ Dart/lib/katas/square.dart
@@ -0,0 +1,3 @@
+int squareSum(List numbers) {
+  // your code here
+}
---
+++ Dart/test/katas/square_test.dart
@@ -0,0 +1,12 @@
+import "package:test/test.dart";
+import "package:solution/solution.dart";
+
+void main() {
+  test('Basic tests', () {
+    expect(squareSum([1,2]), equals(5));
+    expect(squareSum([0,3,4,5]), equals(50));
+    expect(squareSum([]), equals(0));
+    expect(squareSum([-1,-2]), equals(5));
+    expect(squareSum([-1,0,1]), equals(2));
+  });
+}
